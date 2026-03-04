6f676406f6b1453387cac91969a6a54b09f192b7
Sadhavidhadve <1262252509@mitwpu.edu.in>
2026-03-04 15:15:13 +0530

    Create assignment2.c

---
+++ assignment2.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+  float num1, num2, result;
+  printf("enter the two no.s :");
+  scanf("%f,%f",&num1,&num2);
+  printf("addition : %f",num1+num2);
+  printf("substraction : %f",num1-num2);
+  print("division : %f", num1/num2);
+  print("multipilication : %f ",num1*num2);
+return 0;
+}
