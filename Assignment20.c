144222b7c21aefe190bcc530628fca74aab32a46
Sadhavidhadve <1262252509@mitwpu.edu.in>
2026-04-23 10:26:53 +0530

    Create Assignment20.c

---
+++ Assignment20.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+void swapWithPointers(int *a, int *b) {
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+int main() {
+    int a, b, temp;
+    printf("Enter two numbers: ");
+    scanf("%d %d", &a, &b);
+     
+    temp = a; a = b; b = temp;
+    printf("After swap (without pointers): a=%d b=%d\n", a, b);
+   
+    swapWithPointers(&a, &b);
+    printf("After swap (with pointers): a=%d b=%d\n", a, b);
+    return 0;
+}
